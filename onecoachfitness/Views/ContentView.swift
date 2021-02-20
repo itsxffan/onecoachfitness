67f026ba9c8c028cb68170fb1980650728325fde
Saffan Ahmed <saffan_a@icloud.com>
2021-02-20 01:25:22 +0000

    Re-Edit of Navigation Bar

--- onecoachfitness/Views/ContentView.swift
+++ onecoachfitness/Views/ContentView.swift
@@ -11,63 +11,81 @@ import UIKit
 struct ContentView: View {
     
     @State var selectedIndex = 0
-    let tabBarImageNames = ["map", "bolt.heart", "person", "gear"]
-    let tabBarTitleNames = ["Gym", "Exercise", "Profile", "Settings"]
+//    let tabBarImageNames = ["map", "bolt.heart", "person", "gear"]
+//    let tabBarTitleNames = ["Gym", "Exercise", "Profile", "Settings"]
     
     var body: some View {
-        VStack(spacing: 0){
-            ZStack {
-                switch selectedIndex {
-                case 0:
-                    NavigationView {
-                        MapView().scaledToFill()
-                            .navigationTitle("Gym Search")
-                    }
-                
-                case 1:
-                    NavigationView {
-                        ExerciseView()
-                            .navigationTitle("Fitness Plan")
-                    }
-                    
-                case 2:
-                    NavigationView {
-                        ProfileView()
-                            .navigationTitle("Profile")
-                    }
-                    
-                case 3:
-                    NavigationView {
-                        SettingsView()
-                            .navigationTitle("Settings")
-                    }
-                    
-                default:
-                    Text("Remaining Tabs")
-                }
+        VStack {
+            TabView(selection: $selectedIndex) {
+                    Text("Home")
+                        .tag(0)
+                    Text("Gym Search")
+                        .tag(1)
+                    Text("Workout")
+                        .tag(2)
+                    Text("Profile")
+                        .tag(3)
+                    Text("Settings")
+                        .tag(4)
+    //        VStack(spacing: 0){
+    //            ZStack {
+    //                switch selectedIndex {
+    //                case 0:
+    //                    NavigationView {
+    //                        MapView().scaledToFill()
+    //                            .navigationTitle("Gym Search")
+    //                    }
+    //
+    //                case 1:
+    //                    NavigationView {
+    //                        ExerciseView()
+    //                            .navigationTitle("Fitness Plan")
+    //                    }
+    //
+    //                case 2:
+    //                    NavigationView {
+    //                        ProfileView()
+    //                            .navigationTitle("Profile")
+    //                    }
+    //
+    //                case 3:
+    //                    NavigationView {
+    //                        SettingsView()
+    //                            .navigationTitle("Settings")
+    //                    }
+    //
+    //                default:
+    //                    Text("Remaining Tabs")
+    //                }
+    //            }
+    //            //Spacer()
+    //
+    //            Divider()
+    //                .padding(.bottom, 12)
+    //
+    //            HStack {
+    //                ForEach(0..<4) { num in
+    //                    Button(action: {
+    //                        selectedIndex = num
+    //                    }, label: {
+    //                        Spacer()
+    //                        Image(systemName: tabBarImageNames[num])
+    //                            .font(.system(size: 27, weight: .bold))
+    //                            .foregroundColor(selectedIndex == num ? Color(.red):
+    //                                                .init(white: 0.8))
+    //                        //Text(tabBarTitleNames[num])
+    //                        Spacer()
+    //                    })
+    //
+    //                }
+    //            }
+    //
+    //        }
             }
-            //Spacer()
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             
             Divider()
-                .padding(.bottom, 12)
-            
-            HStack {
-                ForEach(0..<4) { num in
-                    Button(action: {
-                        selectedIndex = num
-                    }, label: {
-                        Spacer()
-                        Image(systemName: tabBarImageNames[num])
-                            .font(.system(size: 27, weight: .bold))
-                            .foregroundColor(selectedIndex == num ? Color(.red):
-                                                .init(white: 0.8))
-                        //Text(tabBarTitleNames[num])
-                        Spacer()
-                    })
-                
-                }
-            }
-            
+            // Tab Bar Coming Soon?
         }
     }
 }
